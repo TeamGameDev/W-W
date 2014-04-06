7623853942e76b212691a71c1d4542d116a25032
Jacob Kamieniak <projacobk@gmail.com>
2014-04-06 11:21:51 -0400

    Initial Commit! Added GameManager and two scenes
    MainMenu is the first scene to be loaded, it will have our
    main screen GUI.
    
    TavernTest will have a sample tavern in it to test game mechanics.

---
+++ Assets/Scripts/GameManager.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var level;
+
+function Start () {
+	Debug.Log("GM: Game Manager is up and running :)");
+	level = Application.loadedLevel;
+	Debug.Log("GM: Loaded Scene: " + Application.loadedLevelName);
+}
+
+function Awake () {
+	DontDestroyOnLoad (transform.gameObject);
+}
+
+
+function Update() {
+	if(Application.loadedLevel != level){
+		level = Application.loadedLevel;
+		Debug.Log("GM: Loaded Scene: " + Application.loadedLevelName);
+	}
+}
+
---
+++ Assets/Scripts/MainMenuGUI.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI(){
+
+	var height = Screen.height;
+	var width = Screen.width;
+	
+	GUI.Box(Rect(0,0,Screen.width,Screen.height),"Whiskey and Wizards");
+	
+	if(GUI.Button(Rect(10,10,100,30),"Play")){
+		Application.LoadLevel("TavernTest");
+	}
+
+}//END OnGUI

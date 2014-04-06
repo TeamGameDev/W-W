976d9be7059bff927528269414c593d0ffcf5395
Jacob Kamieniak <projacobk@gmail.com>
2014-04-06 13:07:40 -0400

    TavernTest scene and PLayerController scripts
    also added CameraController script

---
+++ Assets/Scripts/CameraControll.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var player : GameObject;
+private var offset : Vector3;
+
+
+function Start () {
+
+	offset = transform.position;
+}
+
+function LateUpdate () {
+
+	transform.position = offset + player.transform.position;
+
+}
